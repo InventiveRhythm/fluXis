83f5934822673d18891ddac1c3198964b9893d65
Flux <flustixshinu@gmail.com>
2023-01-29 02:45:25 +0100

    Fix background blurring

--- fluXis.Game/Graphics/Background/Background.cs
+++ fluXis.Game/Graphics/Background/Background.cs
@@ -1,3 +1,4 @@
+using fluXis.Game.Configuration;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
@@ -9,62 +10,40 @@ namespace fluXis.Game.Graphics.Background
 {
     public class Background : CompositeDrawable
     {
-        private readonly string backgroundFile;
-        private Sprite backgroundSprite;
+        private readonly string texture;
+        private readonly Sprite sprite;
+        private readonly BufferedContainer buffer;
 
         public Background(string path)
         {
-            backgroundFile = path;
-        }
-
-        [BackgroundDependencyLoader]
-        private void load(TextureStore textures, BackgroundTextureStore backgrounds)
-        {
+            texture = path;
             RelativeSizeAxes = Axes.Both;
-            RelativePositionAxes = Axes.Both;
-            Anchor = Anchor.Centre;
-            Origin = Anchor.Centre;
-            Size = new Vector2(1.1f); // 10% bigger than the screen, used for the Y movement
 
-            // blurring seems like it takes very long to load
-            // i'll try to fix this later
-
-            /*InternalChild = new BufferedContainer(cachedFrameBuffer: true)
+            AddInternal(buffer = new BufferedContainer(cachedFrameBuffer: true)
             {
                 RelativeSizeAxes = Axes.Both,
                 RedrawOnScale = false,
-                FrameBufferScale = new Vector2(10),
-                BlurSigma = new Vector2(100),
-                Child = backgroundSprite = new Sprite
+                Child = sprite = new Sprite
                 {
-                    Texture = backgrounds.Get(backgroundFile) ?? textures.Get(@"Backgrounds/default.png"),
                     RelativeSizeAxes = Axes.Both,
-                    Size = new Vector2(1f),
-                    FillMode = FillMode.Fill,
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
+                    FillMode = FillMode.Fill,
                 }
-            };*/
+            });
+        }
 
-            InternalChild = backgroundSprite = new Sprite
-            {
-                Texture = backgrounds.Get(backgroundFile) ?? textures.Get(@"Backgrounds/default.png"),
-                RelativeSizeAxes = Axes.Both,
-                Size = new Vector2(1f),
-                FillMode = FillMode.Fill,
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-            };
-
+        [BackgroundDependencyLoader]
+        private void load(TextureStore textures, BackgroundTextureStore backgrounds, FluXisConfig config)
+        {
+            sprite.Texture = backgrounds.Get(texture) ?? textures.Get(@"Backgrounds/default.png");
+            buffer.BlurSigma = new Vector2(config.Get<float>(FluXisSetting.BackgroundBlur) * 25);
             Alpha = 0f;
         }
 
         protected override void LoadComplete()
         {
-            this.MoveToY(.05f)
-                .FadeInFromZero(300)
-                .MoveToY(0f, 800, Easing.OutQuint);
-
+            this.FadeInFromZero(300);
             base.LoadComplete();
         }
     }
--- fluXis.Game/Graphics/Background/BackgroundTextureStore.cs
+++ fluXis.Game/Graphics/Background/BackgroundTextureStore.cs
@@ -4,7 +4,7 @@ using osu.Framework.Platform;
 
 namespace fluXis.Game.Graphics.Background
 {
-    public class BackgroundTextureStore : TextureStore
+    public class BackgroundTextureStore : LargeTextureStore
     {
         public BackgroundTextureStore(GameHost host, Storage storage)
             : base(host.Renderer, host.CreateTextureLoaderStore(new StorageBackedResourceStore(storage.GetStorageForDirectory("maps"))))
