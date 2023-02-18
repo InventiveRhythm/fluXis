37bc012cfbe945005bd58767732ccfefed85e667
Flux <flustixshinu@gmail.com>
2023-02-19 00:38:33 +0100

    Add right-click scrolling in songselect

--- fluXis.Game/Screens/Select/List/MapList.cs
+++ fluXis.Game/Screens/Select/List/MapList.cs
@@ -3,84 +3,129 @@ using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Input.Events;
 using osuTK;
+using osuTK.Input;
 
-namespace fluXis.Game.Screens.Select.List
+namespace fluXis.Game.Screens.Select.List;
+
+public partial class MapList : BasicScrollContainer
 {
-    public partial class MapList : BasicScrollContainer
+    private bool isDragging;
+    private bool shouldDrag(MouseButtonEvent e) => e.Button == MouseButton.Right;
+
+    protected override bool IsDragging => base.IsDragging || isDragging;
+
+    [BackgroundDependencyLoader]
+    private void load()
     {
-        [BackgroundDependencyLoader]
-        private void load()
+        Anchor = Anchor.CentreLeft;
+        Origin = Anchor.CentreLeft;
+        RelativeSizeAxes = Axes.Both;
+        Width = .5f;
+        Masking = false;
+        Padding = new MarginPadding(10) { Left = 20 };
+    }
+
+    protected override void Update()
+    {
+        for (var i = 0; i < Content.Children.Count; i++)
         {
-            Anchor = Anchor.CentreLeft;
-            Origin = Anchor.CentreLeft;
-            RelativeSizeAxes = Axes.Both;
-            Width = .5f;
-            Masking = false;
-            Padding = new MarginPadding(10) { Left = 20 };
+            var child = Content.Children[i];
+
+            if (i > 0)
+                child.Y = Content.Children[i - 1].Y + Content.Children[i - 1].Height + 5;
+            else
+                child.Y = 0;
         }
 
-        protected override void Update()
+        base.Update();
+    }
+
+    public void ScrollTo(MapListEntry entry)
+    {
+        var pos1 = GetChildPosInContent(entry);
+        var pos2 = GetChildPosInContent(entry, entry.DrawSize);
+
+        var min = Math.Min(pos1, pos2);
+        var max = Math.Max(pos1, pos2);
+
+        if (min < Current || (min > Current && entry.DrawSize[ScrollDim] > DisplayableContent))
+            ScrollTo(min);
+        else if (max > Current + DisplayableContent)
+            ScrollTo(max - DisplayableContent);
+    }
+
+    private void rightMouseScroll(MouseEvent e)
+    {
+        ScrollTo(Clamp(ToLocalSpace(e.ScreenSpaceMousePosition)[ScrollDim] / DrawSize[ScrollDim]) * DrawSize[ScrollDim], true, 0.02);
+    }
+
+    protected override bool OnMouseDown(MouseDownEvent e)
+    {
+        if (shouldDrag(e))
         {
-            for (var i = 0; i < Content.Children.Count; i++)
-            {
-                var child = Content.Children[i];
-
-                if (i > 0)
-                    child.Y = Content.Children[i - 1].Y + Content.Children[i - 1].Height + 5;
-                else
-                    child.Y = 0;
-            }
-
-            base.Update();
+            rightMouseScroll(e);
+            isDragging = true;
+            return true;
         }
 
-        public void ScrollTo(MapListEntry entry)
+        return base.OnMouseDown(e);
+    }
+
+    protected override void OnMouseUp(MouseUpEvent e)
+    {
+        if (shouldDrag(e))
         {
-            var pos1 = GetChildPosInContent(entry);
-            var pos2 = GetChildPosInContent(entry, entry.DrawSize);
-
-            var min = Math.Min(pos1, pos2);
-            var max = Math.Max(pos1, pos2);
-
-            if (min < Current || (min > Current && entry.DrawSize[ScrollDim] > DisplayableContent))
-                ScrollTo(min);
-            else if (max > Current + DisplayableContent)
-                ScrollTo(max - DisplayableContent);
+            isDragging = false;
+            return;
         }
 
-        protected override ScrollbarContainer CreateScrollbar(Direction direction) => new MapListScrollbar(direction);
+        base.OnMouseUp(e);
+    }
 
-        protected partial class MapListScrollbar : ScrollbarContainer
+    protected override bool OnMouseMove(MouseMoveEvent e)
+    {
+        if (isDragging)
         {
-            public MapListScrollbar(Direction direction)
-                : base(direction)
-            {
-                CornerRadius = 4;
-                Masking = true;
-                Margin = new MarginPadding(2) { Left = -10 };
+            rightMouseScroll(e);
+            return true;
+        }
 
-                Child = new Box
-                {
-                    RelativeSizeAxes = Axes.Both,
-                    Colour = Colour4.FromHex("#222228")
-                };
-            }
+        return base.OnMouseMove(e);
+    }
 
-            protected override void LoadComplete()
-            {
-                Anchor = Origin = Anchor.TopLeft;
-                base.LoadComplete();
-            }
+    protected override ScrollbarContainer CreateScrollbar(Direction direction) => new MapListScrollbar(direction);
 
-            public override void ResizeTo(float val, int duration = 0, Easing easing = Easing.None)
+    protected partial class MapListScrollbar : ScrollbarContainer
+    {
+        public MapListScrollbar(Direction direction)
+            : base(direction)
+        {
+            CornerRadius = 4;
+            Masking = true;
+            Margin = new MarginPadding(2) { Left = -10 };
+
+            Child = new Box
             {
-                Vector2 size = new Vector2(8)
-                {
-                    [(int)ScrollDirection] = val
-                };
-                this.ResizeTo(size, duration, easing);
-            }
+                RelativeSizeAxes = Axes.Both,
+                Colour = Colour4.FromHex("#222228")
+            };
+        }
+
+        protected override void LoadComplete()
+        {
+            Anchor = Origin = Anchor.TopLeft;
+            base.LoadComplete();
+        }
+
+        public override void ResizeTo(float val, int duration = 0, Easing easing = Easing.None)
+        {
+            Vector2 size = new Vector2(8)
+            {
+                [(int)ScrollDirection] = val
+            };
+            this.ResizeTo(size, duration, easing);
         }
     }
 }
