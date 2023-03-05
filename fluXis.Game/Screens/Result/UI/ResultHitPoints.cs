bdd9351a7d9d2ca0f2de09c0628032e46f500f51
Flux <flustixshinu@gmail.com>
2023-03-05 21:44:41 +0100

    Allow zooming into the result hit points

--- fluXis.Game/Screens/Result/UI/ResultHitPoints.cs
+++ fluXis.Game/Screens/Result/UI/ResultHitPoints.cs
@@ -6,74 +6,117 @@ using fluXis.Game.Utils;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Input.Events;
 using osuTK;
+using osuTK.Input;
 
-namespace fluXis.Game.Screens.Result.UI
+namespace fluXis.Game.Screens.Result.UI;
+
+public partial class ResultHitPoints : Container
 {
-    public partial class ResultHitPoints : Container
+    private readonly Container hitPoints;
+
+    public ResultHitPoints(MapInfo map, Performance performance)
     {
-        public ResultHitPoints(MapInfo map, Performance performance)
+        Height = 300;
+        RelativeSizeAxes = Axes.X;
+        Margin = new MarginPadding { Top = 10 };
+        CornerRadius = 10;
+        Masking = true;
+
+        AddRangeInternal(new Drawable[]
         {
-            Height = 300;
-            RelativeSizeAxes = Axes.X;
-            Margin = new MarginPadding { Top = 10 };
-            CornerRadius = 10;
+            new Box
+            {
+                RelativeSizeAxes = Axes.Both,
+                Colour = FluXisColors.Surface
+            },
+            new Container
+            {
+                RelativeSizeAxes = Axes.Both,
+                Padding = new MarginPadding(10),
+                Child = hitPoints = new Container
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                }
+            },
+        });
+
+        foreach (var hitPoint in performance.HitPoints)
+            hitPoints.Add(new ResultHitPoint(map, hitPoint));
+    }
+
+    protected override bool OnDragStart(DragStartEvent e)
+    {
+        if (e.Button == MouseButton.Left)
+            return true;
+
+        return false;
+    }
+
+    protected override void OnDrag(DragEvent e)
+    {
+        if (e.Button == MouseButton.Left)
+        {
+            float maxX = hitPoints.DrawWidth / 2f * hitPoints.Scale.X;
+            hitPoints.X += e.Delta.X;
+            hitPoints.X = MathHelper.Clamp(hitPoints.X, -maxX, maxX);
+
+            float maxY = hitPoints.DrawHeight / 2f * hitPoints.Scale.Y;
+            hitPoints.Y += e.Delta.Y;
+            hitPoints.Y = MathHelper.Clamp(hitPoints.Y, -maxY, maxY);
+        }
+    }
+
+    protected override bool OnScroll(ScrollEvent e)
+    {
+        hitPoints.ScaleTo(MathHelper.Clamp(hitPoints.Scale.X + e.ScrollDelta.Y / 2f, 1f, 5f), 100, Easing.OutQuint);
+        return true;
+    }
+
+    protected override bool OnMouseDown(MouseDownEvent e)
+    {
+        if (e.Button == MouseButton.Right)
+        {
+            hitPoints.ScaleTo(1f, 200, Easing.OutQuint);
+            hitPoints.MoveTo(Vector2.Zero, 200, Easing.OutQuint);
+            return true;
+        }
+
+        return false;
+    }
+
+    public partial class ResultHitPoint : CircularContainer, IHasTooltip
+    {
+        public string Tooltip => TimeUtils.Format(point.Time) + " | " + point.Difference + "ms";
+
+        private readonly HitPoint point;
+
+        public ResultHitPoint(MapInfo map, HitPoint point)
+        {
+            this.point = point;
+
+            Size = new Vector2(3);
             Masking = true;
+            RelativePositionAxes = Axes.X;
+            Anchor = Anchor.CentreLeft;
+            Origin = Anchor.Centre;
 
-            Container hitPoints;
+            X = (point.Time - map.StartTime) / (map.EndTime - map.StartTime);
+            Y = point.Difference;
 
-            AddRangeInternal(new Drawable[]
+            HitWindow hitWindow = HitWindow.FromKey(point.Judgement);
+
+            Children = new Drawable[]
             {
                 new Box
                 {
                     RelativeSizeAxes = Axes.Both,
-                    Colour = FluXisColors.Surface
-                },
-                new Container
-                {
-                    RelativeSizeAxes = Axes.Both,
-                    Padding = new MarginPadding(10),
-                    Child = hitPoints = new Container
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                    }
-                },
-            });
-
-            foreach (var hitPoint in performance.HitPoints)
-                hitPoints.Add(new Dot(map, hitPoint));
-        }
-
-        private partial class Dot : CircularContainer, IHasTooltip
-        {
-            public string Tooltip => TimeUtils.Format(point.Time) + " | " + point.Difference + "ms";
-
-            private readonly HitPoint point;
-
-            public Dot(MapInfo map, HitPoint point)
-            {
-                this.point = point;
-
-                Size = new Vector2(3);
-                Masking = true;
-                RelativePositionAxes = Axes.X;
-                Anchor = Anchor.CentreLeft;
-                Origin = Anchor.Centre;
-
-                X = (point.Time - map.StartTime) / (map.EndTime - map.StartTime);
-                Y = point.Difference;
-
-                HitWindow hitWindow = HitWindow.FromKey(point.Judgement);
-
-                Children = new Drawable[]
-                {
-                    new Box
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        Colour = hitWindow.Color
-                    }
-                };
-            }
+                    Colour = hitWindow.Color
+                }
+            };
         }
     }
 }
