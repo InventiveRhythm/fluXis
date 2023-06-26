a31e143881457f77a483585db8ba98f9a9196005
Flux <flustixshinu@gmail.com>
2023-06-26 22:29:59 +0200

    Android map importing (in the most dumbest way ever)

--- fluXis.Android/FluXisGameAndroid.cs
+++ fluXis.Android/FluXisGameAndroid.cs
@@ -1,7 +1,21 @@
+using System.IO;
 using fluXis.Game;
 
 namespace fluXis.Android;
 
 public partial class FluXisGameAndroid : FluXisGame
 {
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+
+        var importFolder = Host.Storage.GetFullPath("import");
+        if (!Directory.Exists(importFolder)) Directory.CreateDirectory(importFolder);
+
+        var importFiles = Directory.GetFiles(importFolder);
+
+        if (importFiles.Length <= 0) return;
+
+        HandleDragDrop(importFiles);
+    }
 }
--- fluXis.Android/MainActivity.cs
+++ fluXis.Android/MainActivity.cs
@@ -1,20 +1,29 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.Graphics;
 using Android.OS;
 using Android.Views;
+using fluXis.Game;
 using osu.Framework.Android;
 using Debug = System.Diagnostics.Debug;
+using Environment = Android.OS.Environment;
+using Path = System.IO.Path;
 
 namespace fluXis.Android;
 
 [Activity(ConfigurationChanges = DEFAULT_CONFIG_CHANGES, Exported = true, LaunchMode = DEFAULT_LAUNCH_MODE, MainLauncher = true)]
+[IntentFilter(new[] { "android.intent.action.VIEW" }, Categories = new[] { "android.intent.category.DEFAULT" }, DataScheme = "content", DataPathPattern = ".*\\\\.fms", DataHost = "*", DataMimeType = "*/*")]
+[IntentFilter(new[] { "android.intent.action.VIEW" }, Categories = new[] { "android.intent.category.DEFAULT" }, DataScheme = "content", DataMimeType = "application/x-fluXis-mapset-archive")]
 public class MainActivity : AndroidGameActivity
 {
     public ScreenOrientation DefaultOrientation = ScreenOrientation.Unspecified;
 
-    protected override osu.Framework.Game CreateGame() => new FluXisGameAndroid();
+    protected override osu.Framework.Game CreateGame() => game = new FluXisGameAndroid();
+    private FluXisGame game { get; set; }
 
     protected override void OnCreate(Bundle savedInstanceState)
     {
@@ -35,5 +44,28 @@ public class MainActivity : AndroidGameActivity
         float smallestWidthDp = Math.Min(displaySize.X, displaySize.Y) / Resources.DisplayMetrics.Density;
         bool isTablet = smallestWidthDp >= 600f;
         RequestedOrientation = DefaultOrientation = isTablet ? ScreenOrientation.FullUser : ScreenOrientation.SensorLandscape;
+    }
+
+    protected override void OnNewIntent(Intent intent)
+    {
+        Task.Run(() =>
+        {
+            switch (intent?.Action)
+            {
+                case Intent.ActionView:
+                    // move file to import folder
+                    string path = intent.Data?.Path;
+
+                    if (path != null && Environment.DataDirectory != null)
+                    {
+                        string importPath = Path.Combine(Environment.DataDirectory.AbsolutePath, "import");
+                        Directory.CreateDirectory(importPath);
+                        string newPath = Path.Combine(importPath, Path.GetFileName(path));
+                        File.Copy(path, newPath, true);
+                    }
+
+                    break;
+            }
+        });
     }
 }
