eec67a5201fa5f360e2b1ce4a22a27280f9c0e83
Flux <flustixshinu@gmail.com>
2023-07-02 16:57:25 +0200

    Make all values in `FluXisColors` getters

--- fluXis.Game/Graphics/FluXisColors.cs
+++ fluXis.Game/Graphics/FluXisColors.cs
@@ -3,41 +3,41 @@ using osu.Framework.Graphics.Colour;
 
 namespace fluXis.Game.Graphics;
 
-public class FluXisColors
+public static class FluXisColors
 {
-    public static Colour4 Accent = Colour4.FromHex("#3650eb");
-    public static Colour4 Accent2 = Colour4.FromHex("#4846d5");
-    public static Colour4 Accent3 = Colour4.FromHex("#533ec3");
-    public static Colour4 Accent4 = Colour4.FromHex("#5f30a7");
+    public static Colour4 Accent => Colour4.FromHex("#3650eb");
+    public static Colour4 Accent2 => Colour4.FromHex("#4846d5");
+    public static Colour4 Accent3 => Colour4.FromHex("#533ec3");
+    public static Colour4 Accent4 => Colour4.FromHex("#5f30a7");
 
-    public static ColourInfo AccentGradient = ColourInfo.GradientHorizontal(Accent, Accent4);
+    public static ColourInfo AccentGradient => ColourInfo.GradientHorizontal(Accent, Accent4);
 
-    public static Colour4 Background = Colour4.FromHex("#1a1a20");
-    public static Colour4 Background2 = Colour4.FromHex("#222228");
+    public static Colour4 Background => Colour4.FromHex("#1a1a20");
+    public static Colour4 Background2 => Colour4.FromHex("#222228");
 
-    public static Colour4 Hover = Colour4.FromHex("#7e7e7f");
-    public static Colour4 Click = Colour4.FromHex("#ffffff");
+    public static Colour4 Hover => Colour4.FromHex("#7e7e7f");
+    public static Colour4 Click => Colour4.FromHex("#ffffff");
 
-    public static Colour4 Surface = Colour4.FromHex("#2a2a30");
-    public static Colour4 Surface2 = Colour4.FromHex("#323238");
-    public static Colour4 SurfaceDisabled = Colour4.FromHex("#2e2e34");
+    public static Colour4 Surface => Colour4.FromHex("#2a2a30");
+    public static Colour4 Surface2 => Colour4.FromHex("#323238");
+    public static Colour4 SurfaceDisabled => Colour4.FromHex("#2e2e34");
 
-    public static Colour4 Text = Colour4.FromHex("#ffffff");
-    public static Colour4 Text2 = Colour4.FromHex("#c8c8c8");
-    public static Colour4 TextDisabled = Colour4.FromHex("#646464");
-    public static Colour4 TextDark = Colour4.FromHex("#1a1a20");
+    public static Colour4 Text => Colour4.FromHex("#ffffff");
+    public static Colour4 Text2 => Colour4.FromHex("#c8c8c8");
+    public static Colour4 TextDisabled => Colour4.FromHex("#646464");
+    public static Colour4 TextDark => Colour4.FromHex("#1a1a20");
 
-    public static Colour4 RoleAdmin = Colour4.FromHex("#f7b373");
-    public static Colour4 RoleMod = Colour4.FromHex("#73d173");
-    public static Colour4 RolePurifier = Colour4.FromHex("#55b2ff");
-    public static Colour4 RoleFeatured = Colour4.FromHex("#ff7b74");
-    public static Colour4 RoleUser = Colour4.FromHex("#7455ff");
-    public static Colour4 RoleBot = Colour4.FromHex("#1f1e33");
+    public static Colour4 RoleAdmin => Colour4.FromHex("#f7b373");
+    public static Colour4 RoleMod => Colour4.FromHex("#73d173");
+    public static Colour4 RolePurifier => Colour4.FromHex("#55b2ff");
+    public static Colour4 RoleFeatured => Colour4.FromHex("#ff7b74");
+    public static Colour4 RoleUser => Colour4.FromHex("#7455ff");
+    public static Colour4 RoleBot => Colour4.FromHex("#1f1e33");
 
-    public static Colour4 SocialTwitter = Colour4.FromHex("#1da1f2");
-    public static Colour4 SocialYoutube = Colour4.FromHex("#ff0000");
-    public static Colour4 SocialTwitch = Colour4.FromHex("#6441a5");
-    public static Colour4 SocialDiscord = Colour4.FromHex("#7289da");
+    public static Colour4 SocialTwitter => Colour4.FromHex("#1da1f2");
+    public static Colour4 SocialYoutube => Colour4.FromHex("#ff0000");
+    public static Colour4 SocialTwitch => Colour4.FromHex("#6441a5");
+    public static Colour4 SocialDiscord => Colour4.FromHex("#7289da");
 
     public static bool IsBright(Colour4 color)
     {
