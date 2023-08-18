f1c2810dc61c7ff41ab18dec8e2f37317560fea5
Flux <flustixshinu@gmail.com>
2023-08-18 20:43:29 +0200

    Change loading icon

---
+++ fluXis.Game.Tests/Graphics/TestLoadingIcon.cs
@@ -0,0 +1,27 @@
+using fluXis.Game.Graphics;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osuTK;
+
+namespace fluXis.Game.Tests.Graphics;
+
+public partial class TestLoadingIcon : FluXisTestScene
+{
+    public TestLoadingIcon()
+    {
+        AddRange(new Drawable[]
+        {
+            new Box
+            {
+                RelativeSizeAxes = Axes.Both,
+                Colour = FluXisColors.Background3
+            },
+            new LoadingIcon
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(100)
+            }
+        });
+    }
+}
