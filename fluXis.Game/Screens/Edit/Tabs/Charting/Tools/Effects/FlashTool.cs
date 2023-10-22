552e7fefd2db56cdb9a2e2a20eb88fbbb5ee51c9
Flux <flustixshinu@gmail.com>
2023-10-22 06:53:19 +0200

    Add new icons for effects

--- fluXis.Game/Screens/Edit/Tabs/Charting/Tools/Effects/FlashTool.cs
+++ fluXis.Game/Screens/Edit/Tabs/Charting/Tools/Effects/FlashTool.cs
@@ -1,15 +1,15 @@
+using fluXis.Game.Graphics.Sprites;
 using fluXis.Game.Screens.Edit.Tabs.Charting.Placement;
 using fluXis.Game.Screens.Edit.Tabs.Charting.Placement.Effect;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Sprites;
 
 namespace fluXis.Game.Screens.Edit.Tabs.Charting.Tools.Effects;
 
 public class FlashTool : EffectTool
 {
     public override string Name => "Flash";
-    public override string Description => "Creates a screen flash that fills the entire screen.";
+    public override string Description => "Creates a flash that fills the entire screen.";
     public override string Letter => "F";
-    public override Drawable CreateIcon() => new SpriteIcon { Icon = FontAwesome.Solid.Lightbulb };
+    public override Drawable CreateIcon() => new FluXisIcon { Type = FluXisIconType.Flash };
     public override PlacementBlueprint CreateBlueprint() => new FlashPlacementBlueprint();
 }
--- fluXis.Game/Screens/Edit/Tabs/Charting/Tools/Effects/LaneSwitchTool.cs
+++ fluXis.Game/Screens/Edit/Tabs/Charting/Tools/Effects/LaneSwitchTool.cs
@@ -1,7 +1,7 @@
+using fluXis.Game.Graphics.Sprites;
 using fluXis.Game.Screens.Edit.Tabs.Charting.Placement;
 using fluXis.Game.Screens.Edit.Tabs.Charting.Placement.Effect;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Sprites;
 
 namespace fluXis.Game.Screens.Edit.Tabs.Charting.Tools.Effects;
 
@@ -10,6 +10,6 @@ public class LaneSwitchTool : EffectTool
     public override string Name => "Lane Switch";
     public override string Description => "Changes the amount of lanes mid-song.";
     public override string Letter => "L";
-    public override Drawable CreateIcon() => new SpriteIcon { Icon = FontAwesome.Solid.ArrowsAltH };
+    public override Drawable CreateIcon() => new FluXisIcon { Type = FluXisIconType.LaneSwitch };
     public override PlacementBlueprint CreateBlueprint() => new LaneSwitchPlacementBlueprint();
 }
