e65cac23a98c82d002d3f7e82d72438b7f953170
Flux <flustixshinu@gmail.com>
2023-10-31 06:30:35 +0100

    Add scale animations to context menus

--- fluXis.Game/Graphics/UserInterface/Context/FluXisContextMenu.cs
+++ fluXis.Game/Graphics/UserInterface/Context/FluXisContextMenu.cs
@@ -1,5 +1,6 @@
 using fluXis.Game.Graphics.UserInterface.Menu;
 using osu.Framework.Graphics;
+using osuTK;
 
 namespace fluXis.Game.Graphics.UserInterface.Context;
 
@@ -9,6 +10,18 @@ public partial class FluXisContextMenu : FluXisMenu
         : base(Direction.Vertical)
     {
         MaskingContainer.EdgeEffect = FluXisStyles.ShadowSmall;
+    }
+
+    protected override void UpdateSize(Vector2 newSize)
+    {
+        if (newSize.Y == 0)
+            this.ScaleTo(.8f, 400, Easing.OutQuint);
+        else
+        {
+            Size = newSize;
+            Width = newSize.X + 30;
+            this.ScaleTo(1, 1000, Easing.OutElastic);
+        }
     }
 
     protected override osu.Framework.Graphics.UserInterface.Menu CreateSubMenu() => new FluXisContextMenu();
