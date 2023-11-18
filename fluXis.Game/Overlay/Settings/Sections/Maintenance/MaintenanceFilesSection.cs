bcb1d355227213979576384677d62e274076d00d
Flux <flustixshinu@gmail.com>
2023-11-18 04:32:59 +0100

    Implement file cleanup

--- fluXis.Game/Overlay/Settings/Sections/Maintenance/MaintenanceFilesSection.cs
+++ fluXis.Game/Overlay/Settings/Sections/Maintenance/MaintenanceFilesSection.cs
@@ -1,6 +1,14 @@
+using System;
+using System.IO;
+using System.Linq;
+using fluXis.Game.Map;
+using fluXis.Game.Overlay.Notifications;
 using fluXis.Game.Overlay.Settings.UI;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Logging;
+using osu.Framework.Platform;
 
 namespace fluXis.Game.Overlay.Settings.Sections.Maintenance;
 
@@ -9,7 +17,7 @@ public partial class MaintenanceFilesSection : SettingsSubSection
     public override string Title => "Files";
 
     [BackgroundDependencyLoader]
-    private void load()
+    private void load(Storage storage, MapStore store, NotificationManager notifications)
     {
         AddRange(new Drawable[]
         {
@@ -17,8 +25,34 @@ public partial class MaintenanceFilesSection : SettingsSubSection
             {
                 Label = "Clean up files",
                 Description = "Deletes all files that are not used by any maps",
-                Enabled = false,
-                ButtonText = "Run"
+                ButtonText = "Run",
+                Action = () =>
+                {
+                    notifications.SendSmallText("Cleaning up files...", FontAwesome.Solid.Sync);
+                    var deleted = 0;
+                    var errors = 0;
+
+                    foreach (var directory in storage.GetDirectories("maps"))
+                    {
+                        var guid = directory.Split(Path.DirectorySeparatorChar).Last();
+
+                        if (store.MapSets.All(m => m.ID.ToString() != guid))
+                        {
+                            try
+                            {
+                                storage.DeleteDirectory(directory);
+                                deleted++;
+                            }
+                            catch (Exception ex)
+                            {
+                                errors++;
+                                Logger.Error(ex, $"Failed to delete directory {directory}");
+                            }
+                        }
+                    }
+
+                    notifications.SendText($"Cleaned up {deleted} folder(s)", errors != 0 ? $"{errors} deletion(s) failed." : "", FontAwesome.Solid.Check);
+                }
             }
         });
     }
