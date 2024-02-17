ea551927c5dc2795d66395428572f65ca7485679
Flux <flustix@foxes4life.net>
2024-02-17 23:36:48 +0100

    Enable the update performer again

--- fluXis.Desktop/FluXisGameDesktop.cs
+++ fluXis.Desktop/FluXisGameDesktop.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Linq;
 using fluXis.Desktop.Integration;
 using fluXis.Game;
 using fluXis.Game.Integration;
 using fluXis.Game.IPC;
+using fluXis.Game.Updater;
 using osu.Framework.Allocation;
 using osu.Framework.Platform;
 
@@ -38,5 +40,5 @@ public partial class FluXisGameDesktop : FluXisGame
     }
 
     public override LightController CreateLightController() => new OpenRGBController();
-    // public override IUpdatePerformer CreateUpdatePerformer() => OperatingSystem.IsWindows() ? new WindowsUpdatePerformer(NotificationManager) : null;
+    public override IUpdatePerformer CreateUpdatePerformer() => OperatingSystem.IsWindows() ? new WindowsUpdatePerformer(NotificationManager) : null;
 }
