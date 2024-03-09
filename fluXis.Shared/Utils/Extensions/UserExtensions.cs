90554cbdfcba4b469f6dee9525199c10e638f702
Flux <flustix@foxes4life.net>
2024-03-09 08:52:26 +0100

    Add `UserExtensions.IsPurifier()`

--- fluXis.Shared/Utils/Extensions/UserExtensions.cs
+++ fluXis.Shared/Utils/Extensions/UserExtensions.cs
@@ -4,10 +4,18 @@ namespace fluXis.Shared.Utils.Extensions;
 
 public static class UserExtensions
 {
-    public static bool IsDeveloper(this APIUserShort user)
+    public static bool IsDeveloper(this IAPIUserShort user)
         => user.Groups.Any(g => g.ID == "dev");
 
-    public static bool CanModerate(this APIUserShort user)
+    public static bool IsPurifier(this IAPIUserShort user)
+    {
+        if (user.IsDeveloper())
+            return true;
+
+        return user.Groups.Any(g => g.ID == "purifier");
+    }
+
+    public static bool CanModerate(this IAPIUserShort user)
     {
         if (user.IsDeveloper())
             return true;
