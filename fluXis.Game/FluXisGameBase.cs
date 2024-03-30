86930cbcff9e15eeb5251c3345df070b244c1d4c
flustix <me@flux.moe>
2024-03-30 14:36:13 +0100

    remove the comment that has been there since forever

--- fluXis.Game/FluXisGameBase.cs
+++ fluXis.Game/FluXisGameBase.cs
@@ -62,10 +62,6 @@ namespace fluXis.Game;
 
 public partial class FluXisGameBase : osu.Framework.Game
 {
-    // Anything in this class is shared between the test browser and the game implementation.
-    // It allows for caching global dependencies that should be accessible to tests, or changing
-    // the screen scaling for all components including the test browser and framework overlays.
-
     protected DependencyContainer GameDependencies { get; private set; }
 
     public Vector2 ContentSize => content.DrawSize;
