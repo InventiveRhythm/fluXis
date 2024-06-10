0bf3125e15e781210a8a5c832af7c7108f8bb859
flustix <me@flux.moe>
2024-06-10 02:26:38 +0200

    add toggle to show background video in design tab

--- fluXis.Game/Screens/Edit/Tabs/Design/DesignContainer.cs
+++ fluXis.Game/Screens/Edit/Tabs/Design/DesignContainer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using fluXis.Game.Graphics.Background;
 using fluXis.Game.Screens.Edit.Tabs.Design.Effects;
 using fluXis.Game.Screens.Edit.Tabs.Design.Playfield;
 using fluXis.Game.Screens.Edit.Tabs.Design.Points;
@@ -7,19 +8,58 @@ using fluXis.Game.Screens.Edit.Tabs.Shared;
 using fluXis.Game.Screens.Edit.Tabs.Shared.Points;
 using fluXis.Game.Screens.Edit.Tabs.Shared.Toolbox;
 using osu.Framework.Graphics;
+using osu.Framework.Input.Events;
+using osuTK.Input;
 
 namespace fluXis.Game.Screens.Edit.Tabs.Design;
 
 public partial class DesignContainer : EditorTabContainer
 {
+    private BackgroundVideo backgroundVideo;
+    private bool showingVideo;
+
     protected override IEnumerable<Drawable> CreateContent()
     {
         return new Drawable[]
         {
+            backgroundVideo = new BackgroundVideo
+            {
+                RelativeSizeAxes = Axes.Both,
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                VideoClock = EditorClock,
+                ShowDim = false
+            },
             new EditorFlashLayer { InBackground = true },
             new EditorDesignPlayfield(),
             new EditorFlashLayer()
         };
+    }
+
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+
+        backgroundVideo.Map = Map.RealmMap;
+        backgroundVideo.Info = Map.MapInfo;
+        backgroundVideo.LoadVideo();
+    }
+
+    protected override bool OnKeyDown(KeyDownEvent e)
+    {
+        if (e.Key == Key.V)
+        {
+            showingVideo = !showingVideo;
+
+            if (showingVideo)
+                backgroundVideo.Start();
+            else
+                backgroundVideo.Stop();
+
+            return true;
+        }
+
+        return base.OnKeyDown(e);
     }
 
     protected override EditorToolbox CreateToolbox() => new DesignToolbox();
