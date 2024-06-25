f2e15e349aace038f38422b59eb6988d4856bbcc
flustix <me@flux.moe>
2024-06-25 05:45:09 +0200

    Add club invite stuff to shared lib

---
+++ fluXis.Shared/API/Parameters/Invites/CreateClubInviteParameters.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace fluXis.Shared.API.Parameters.Invites;
+
+public class CreateClubInviteParameters
+{
+    /// <summary>
+    /// The user to invite.
+    /// </summary>
+    [JsonProperty("user")]
+    public long? UserID { get; set; }
+}
