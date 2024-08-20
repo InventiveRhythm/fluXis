62eaceee08bc838e96787f2127720949d8c479b4
flustix <me@flux.moe>
2024-08-20 07:01:04 +0200

    add password reset payload

---
+++ fluXis.Shared/API/Payloads/Auth/PasswordResetPayload.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace fluXis.Shared.API.Payloads.Auth;
+
+public class PasswordResetPayload
+{
+    [JsonProperty("token")]
+    public string? Token { get; set; }
+
+    [JsonProperty("password")]
+    public string? Password { get; set; }
+}
