65a7491e442f53880308f0be773a4e34cccd531a
flustix <me@flux.moe>
2024-11-02 16:44:54 +0100

    add score and rank to club stats

--- fluXis.Shared/Components/Clubs/APIClubStatistics.cs
+++ fluXis.Shared/Components/Clubs/APIClubStatistics.cs
@@ -7,6 +7,12 @@ public class APIClubStatistics
     [JsonProperty("ovr")]
     public double OverallRating { get; set; }
 
+    [JsonProperty("score")]
+    public double TotalScore { get; set; }
+
+    [JsonProperty("rank")]
+    public double Rank { get; set; }
+
     [JsonProperty("claims")]
     public long TotalClaims { get; set; }
 
