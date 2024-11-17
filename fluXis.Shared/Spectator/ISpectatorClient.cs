9e34be27a8403fe1f9aa4e5ec43bf42ac49c79af
flustix <me@flux.moe>
2024-11-17 23:16:55 +0100

    very early spectator stuff

---
+++ fluXis.Shared/Spectator/ISpectatorClient.cs
@@ -0,0 +1,9 @@
+﻿namespace fluXis.Shared.Spectator;
+
+public interface ISpectatorClient
+{
+    Task StartedPlaying(long id, SpectatorState state);
+    Task StoppedPlaying(long id, SpectatorState state);
+    Task RecieveFrameBundle(long id, SpectatorState state);
+    Task ScoreProcessed(long id, SpectatorState state);
+}
