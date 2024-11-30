42d54e182a9627c847e9fefb761429a2ce1eeae0
flustix <me@flux.moe>
2024-11-30 12:01:26 +0100

    fix timing point waveform breaking when both point and current time are 0

--- fluXis.Game/Screens/Edit/Tabs/Shared/Points/Settings/Waveform/WaveformDisplay.cs
+++ fluXis.Game/Screens/Edit/Tabs/Shared/Points/Settings/Waveform/WaveformDisplay.cs
@@ -9,6 +9,7 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Audio;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Logging;
 using osuTK;
 
 namespace fluXis.Game.Screens.Edit.Tabs.Shared.Points.Settings.Waveform;
@@ -25,7 +26,7 @@ public partial class WaveformDisplay : Container
 
     private TimingPoint point { get; }
 
-    private double currentTime;
+    private double currentTime = -10;
     private double endTime;
 
     private int lastIndex = -1;
@@ -99,6 +100,7 @@ public partial class WaveformDisplay : Container
     private void regenerate()
     {
         var index = (int)Math.Round((currentTime - point.Time) / point.MsPerBeat);
+        Logger.Log($"{index}");
 
         // 10ms leniency
         if (clock.IsRunning && lastIndex != index && clock.CurrentTime > point.Time - 10 && clock.CurrentTime < endTime + 10)
