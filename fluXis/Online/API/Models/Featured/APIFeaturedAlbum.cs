3e8749d940813103951de48aa925dcd0c3744bd7
flustix <me@flux.moe>
2025-03-15 01:13:43 +0100

    add fa api models

---
+++ fluXis/Online/API/Models/Featured/APIFeaturedAlbum.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace fluXis.Online.API.Models.Featured;
+
+public class APIFeaturedAlbum
+{
+    [JsonProperty("id")]
+    public string ID { get; set; } = null!;
+
+    [JsonProperty("name")]
+    public string Name { get; set; } = null!;
+
+    [JsonProperty("release")]
+    public AlbumRelease Release { get; set; } = null!;
+
+    [JsonProperty("colors")]
+    public AlbumColors Colors { get; set; } = null!;
+
+    [JsonProperty("tracks")]
+    public List<APIFeaturedTrack> Tracks { get; set; } = new();
+
+    public class AlbumRelease
+    {
+        [JsonProperty("year")]
+        public int Year { get; set; }
+
+        [JsonProperty("month")]
+        public int Month { get; set; }
+
+        [JsonProperty("day")]
+        public int Day { get; set; }
+    }
+
+    public class AlbumColors
+    {
+        [JsonProperty("accent")]
+        public string Accent { get; set; } = "#ffffff";
+
+        [JsonProperty("text")]
+        public string Text { get; set; } = "#ffffff";
+
+        [JsonProperty("text2")]
+        public string Text2 { get; set; } = "#ffffff";
+
+        [JsonProperty("bg")]
+        public string Background { get; set; } = "#ffffff";
+
+        [JsonProperty("bg2")]
+        public string Background2 { get; set; } = "#ffffff";
+    }
+}
---
+++ fluXis/Online/API/Models/Featured/APIFeaturedArtist.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace fluXis.Online.API.Models.Featured;
+
+#nullable enable
+
+public class APIFeaturedArtist
+{
+    [JsonProperty("id")]
+    public string ID { get; set; } = null!;
+
+    [JsonProperty("name")]
+    public string Name { get; set; } = null!;
+
+    [JsonProperty("description")]
+    public string Description { get; set; } = string.Empty;
+
+    [JsonProperty("youtube")]
+    public string? YouTube { get; set; }
+
+    [JsonProperty("spotify")]
+    public string? Spotify { get; set; }
+
+    [JsonProperty("soundcloud")]
+    public string? SoundCloud { get; set; }
+
+    [JsonProperty("twitter")]
+    public string? Twitter { get; set; }
+
+    [JsonProperty("fluxis")]
+    public string? FluXis { get; set; }
+
+    [JsonProperty("albums")]
+    public List<APIFeaturedAlbum> Albums { get; set; } = new();
+}
