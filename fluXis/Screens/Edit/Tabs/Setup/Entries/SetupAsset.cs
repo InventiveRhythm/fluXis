63683cf41cebf9e054e76d3d8417fc05a6f57c14
flustix <me@flux.moe>
2025-05-10 03:53:38 +0200

    allow settings mapset assets by dropping them over the textboxes

--- fluXis/Screens/Edit/Tabs/Setup/Entries/SetupAsset.cs
+++ fluXis/Screens/Edit/Tabs/Setup/Entries/SetupAsset.cs
@@ -4,6 +4,7 @@ using fluXis.Database;
 using fluXis.Graphics.Sprites;
 using fluXis.Graphics.UserInterface.Files;
 using fluXis.Graphics.UserInterface.Panel;
+using fluXis.Utils;
 using JetBrains.Annotations;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
@@ -11,7 +12,7 @@ using osu.Framework.Input.Events;
 
 namespace fluXis.Screens.Edit.Tabs.Setup.Entries;
 
-public partial class SetupAsset : SetupEntry
+public partial class SetupAsset : SetupEntry, IDragDropHandler
 {
     protected override float ContentSpacing => -3;
 
@@ -20,9 +21,13 @@ public partial class SetupAsset : SetupEntry
     private PanelContainer panels { get; set; }
 
     [Resolved]
+    private FluXisGameBase game { get; set; }
+
+    [Resolved]
     private EditorMap map { get; set; }
 
     public string[] AllowedExtensions { get; init; } = Array.Empty<string>();
+
     public Action<FileInfo> OnChange { get; init; } = _ => { };
 
     public string Path
@@ -54,16 +59,13 @@ public partial class SetupAsset : SetupEntry
     {
     }
 
-    protected override Drawable CreateContent()
+    protected override Drawable CreateContent() => text = new TruncatingText
     {
-        return text = new TruncatingText
-        {
-            RelativeSizeAxes = Axes.X,
-            Text = path,
-            WebFontSize = 18,
-            Alpha = empty ? .6f : 1
-        };
-    }
+        RelativeSizeAxes = Axes.X,
+        Text = path,
+        WebFontSize = 18,
+        Alpha = empty ? .6f : 1
+    };
 
     protected override bool OnClick(ClickEvent e)
     {
@@ -74,13 +76,30 @@ public partial class SetupAsset : SetupEntry
         {
             AllowedExtensions = AllowedExtensions,
             MapDirectory = MapFiles.GetFullPath($"{map.RealmMap.MapSet.ID}"),
-            OnFileSelected = file =>
-            {
-                Path = file.Name;
-                OnChange.Invoke(file);
-            }
+            OnFileSelected = setPath
         };
 
         return true;
     }
+
+    private void setPath(FileInfo file)
+    {
+        Path = file.Name;
+        OnChange.Invoke(file);
+    }
+
+    public bool OnDragDrop(string file)
+    {
+        var info = new FileInfo(file);
+        setPath(info);
+        return true;
+    }
+
+    protected override bool OnHover(HoverEvent e)
+    {
+        Schedule(() => game.AddDragDropHandler(this));
+        return true;
+    }
+
+    protected override void OnHoverLost(HoverLostEvent e) => game.RemoveDragDropHandler(this);
 }
