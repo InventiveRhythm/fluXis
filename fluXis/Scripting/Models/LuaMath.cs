4576caaac010af9da28f5ee69cd68ecf223ff2b6
menvae <menvaedev@gmail.com>
2025-08-07 19:31:04 +0300

    expand math library for storyboarding

--- fluXis/Scripting/Models/LuaMath.cs
+++ fluXis/Scripting/Models/LuaMath.cs
@@ -7,15 +7,126 @@ namespace fluXis.Scripting.Models;
 [LuaDefinition("math", Name = "mathf", Public = true)]
 public class LuaMath : ILuaModel
 {
+    // constants
     [LuaMember(Name = "pi")]
     public double Pi => Math.PI;
+
+    [LuaMember(Name = "tau")]
+    public double Tau => Math.Tau;
+
+    [LuaMember(Name = "e")]
+    public double E => Math.E;
+
+    // helper functions
+    [LuaMember(Name = "abs")]
+    public double Abs(double d) => Math.Abs(d);
+
+    [LuaMember(Name = "clamp")]
+    public double Clamp(double value, double min, double max) => Math.Clamp(value, min, max);
+
+    [LuaMember(Name = "min")]
+    public double Min(double a, double b) => Math.Min(a, b);
+
+    [LuaMember(Name = "max")]
+    public double Max(double a, double b) => Math.Max(a, b);
+
+    [LuaMember(Name = "sign")]
+    public int Sign(double d) => Math.Sign(d);
+
+    [LuaMember(Name = "floor")]
+    public double Floor(double d) => Math.Floor(d);
+
+    [LuaMember(Name = "floort")]
+    public double FloorThreshold(double d, double threshold) => d >= threshold ? Math.Floor(d) : d;
+
+    [LuaMember(Name = "ceil")]
+    public double Ceil(double d) => Math.Ceiling(d);
+
+    [LuaMember(Name = "ceilt")]
+    public double CeilThreshold(double d, double threshold) => d <= threshold ? Math.Ceiling(d) : d;
+
+    [LuaMember(Name = "round")]
+    public double Round(double d) => Math.Round(d);
+
+    [LuaMember(Name = "roundt")]
+    public double RoundThreshold(double d, double threshold) => Math.Abs(d - Math.Round(d)) <= threshold ? Math.Round(d) : d;
+
+    [LuaMember(Name = "sqrt")]
+    public double Sqrt(double d) => Math.Sqrt(d);
+
+    [LuaMember(Name = "pow")]
+    public double Pow(double x, double y) => Math.Pow(x, y);
+
+    [LuaMember(Name = "exp")]
+    public double Exp(double d) => Math.Exp(d);
+
+    [LuaMember(Name = "log")]
+    public double Log(double d) => Math.Log(d);
+
+    [LuaMember(Name = "log10")]
+    public double Log10(double d) => Math.Log10(d);
+
+    [LuaMember(Name = "lerp")]
+    public double Lerp(double a, double b, double t) => a + (b - a) * t;
+
+    // trigonometric functions
+    [LuaMember(Name = "sin")]
+    public double Sin(double d) => Math.Sin(d);
 
     [LuaMember(Name = "cos")]
     public double Cos(double d) => Math.Cos(d);
 
-    [LuaMember(Name = "atan2")]
-    public double Atan2(double x, double y) => Math.Atan2(x, y);
+    [LuaMember(Name = "tan")]
+    public double Tan(double d) => Math.Tan(d);
 
+    [LuaMember(Name = "asin")]
+    public double Asin(double d) => Math.Asin(d);
+
+    [LuaMember(Name = "acos")]
+    public double Acos(double d) => Math.Acos(d);
+
+    [LuaMember(Name = "atan")]
+    public double Atan(double d) => Math.Atan(d);
+
+    [LuaMember(Name = "atan2")]
+    public double Atan2(double y, double x) => Math.Atan2(y, x);
+
+    [LuaMember(Name = "deg")]
+    public double Degrees(double radians) => radians * (180.0 / Math.PI);
+
+    [LuaMember(Name = "rad")]
+    public double Radians(double degrees) => degrees * (Math.PI / 180.0);
+
+    // hyperbolic functions
+    [LuaMember(Name = "sinh")]
+    public double Sinh(double d) => Math.Sinh(d);
+
+    [LuaMember(Name = "cosh")]
+    public double Cosh(double d) => Math.Cosh(d);
+
+    [LuaMember(Name = "tanh")]
+    public double Tanh(double d) => Math.Tanh(d);
+
+    // vector operations
     [LuaMember(Name = "vecsub")]
     public LuaVector2 VecSub(LuaVector2 a, LuaVector2 b) => new(a.TKVector - b.TKVector);
+
+    [LuaMember(Name = "vecadd")]
+    public LuaVector2 VecAdd(LuaVector2 a, LuaVector2 b) => new(a.TKVector + b.TKVector);
+
+    [LuaMember(Name = "vecmul")]
+    public LuaVector2 VecMul(LuaVector2 a, double scalar) => new(a.TKVector * (float)scalar);
+
+    [LuaMember(Name = "vecdot")]
+    public double VecDot(LuaVector2 a, LuaVector2 b) => a.TKVector.X * b.TKVector.X + a.TKVector.Y * b.TKVector.Y;
+
+    [LuaMember(Name = "veclen")]
+    public double VecLength(LuaVector2 v) => Math.Sqrt(v.TKVector.X * v.TKVector.X + v.TKVector.Y * v.TKVector.Y);
+
+    [LuaMember(Name = "vecnorm")]
+    public LuaVector2 VecNormalize(LuaVector2 v)
+    {
+        var len = VecLength(v);
+        return len > 0 ? new LuaVector2(v.TKVector / (float)len) : v;
+    }
 }
--- scripting/library/math.lua
+++ scripting/library/math.lua
@@ -2,21 +2,196 @@
 
 ---@class mathf
 ---@field pi number
+---@field tau number
+---@field e number
 mathf = {}
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:abs(d) end
+
+---@param value number
+---@param min number
+---@param max number
+---@return number
+---@nodiscard
+function mathf:clamp(value, min, max) end
+
+---@param a number
+---@param b number
+---@return number
+---@nodiscard
+function mathf:min(a, b) end
+
+---@param a number
+---@param b number
+---@return number
+---@nodiscard
+function mathf:max(a, b) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:sign(d) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:floor(d) end
+
+---@param d number
+---@param threshold number
+---@return number
+---@nodiscard
+function mathf:floort(d, threshold) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:ceil(d) end
+
+---@param d number
+---@param threshold number
+---@return number
+---@nodiscard
+function mathf:ceilt(d, threshold) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:round(d) end
+
+---@param d number
+---@param threshold number
+---@return number
+---@nodiscard
+function mathf:roundt(d, threshold) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:sqrt(d) end
+
+---@param x number
+---@param y number
+---@return number
+---@nodiscard
+function mathf:pow(x, y) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:exp(d) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:log(d) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:log10(d) end
+
+---@param a number
+---@param b number
+---@param t number
+---@return number
+---@nodiscard
+function mathf:lerp(a, b, t) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:sin(d) end
 
 ---@param d number
 ---@return number
 ---@nodiscard
 function mathf:cos(d) end
 
----@param x number
----@param y number
+---@param d number
 ---@return number
 ---@nodiscard
-function mathf:atan2(x, y) end
+function mathf:tan(d) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:asin(d) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:acos(d) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:atan(d) end
+
+---@param y number
+---@param x number
+---@return number
+---@nodiscard
+function mathf:atan2(y, x) end
+
+---@param radians number
+---@return number
+---@nodiscard
+function mathf:deg(radians) end
+
+---@param degrees number
+---@return number
+---@nodiscard
+function mathf:rad(degrees) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:sinh(d) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:cosh(d) end
+
+---@param d number
+---@return number
+---@nodiscard
+function mathf:tanh(d) end
 
 ---@param a Vector2
 ---@param b Vector2
 ---@return Vector2
 ---@nodiscard
 function mathf:vecsub(a, b) end
+
+---@param a Vector2
+---@param b Vector2
+---@return Vector2
+---@nodiscard
+function mathf:vecadd(a, b) end
+
+---@param a Vector2
+---@param scalar number
+---@return Vector2
+---@nodiscard
+function mathf:vecmul(a, scalar) end
+
+---@param a Vector2
+---@param b Vector2
+---@return number
+---@nodiscard
+function mathf:vecdot(a, b) end
+
+---@param v Vector2
+---@return number
+---@nodiscard
+function mathf:veclen(v) end
+
+---@param v Vector2
+---@return Vector2
+---@nodiscard
+function mathf:vecnorm(v) end
