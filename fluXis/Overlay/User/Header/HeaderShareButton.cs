f4fec6e3e2ab9dac026e1e49bac463aee0b587f1
menvae <menvaedev@gmail.com>
2025-08-08 07:00:39 +0300

    implement the share button for profile

---
+++ fluXis/Overlay/User/Header/HeaderShareButton.cs
@@ -0,0 +1,8 @@
+using fluXis.Graphics.Sprites.Icons;
+
+namespace fluXis.Overlay.User.Header;
+
+public partial class HeaderShareButton : HeaderButton
+{
+    public HeaderShareButton() => Icon = FontAwesome6.Solid.ShareNodes;
+}
