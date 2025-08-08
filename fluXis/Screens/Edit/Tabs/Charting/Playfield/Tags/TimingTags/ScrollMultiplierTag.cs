7e2e80200b949eaac869df46b4fabae37a1c6f33
menvae <menvaedev@gmail.com>
2025-08-09 02:50:38 +0300

    hover over ScrollMult tag to see easing info

--- fluXis/Screens/Edit/Tabs/Charting/Playfield/Tags/TimingTags/ScrollMultiplierTag.cs
+++ fluXis/Screens/Edit/Tabs/Charting/Playfield/Tags/TimingTags/ScrollMultiplierTag.cs
@@ -18,15 +18,44 @@ public partial class ScrollMultiplierTag : EditorTag
 
     public ScrollMultiplierEvent ScrollMultiplier => (ScrollMultiplierEvent)TimedObject;
 
+    private bool isHovered;
+    private MarginPadding originalPadding;
+    private MarginPadding expandedPadding;
+
     public ScrollMultiplierTag(EditorTagContainer parent, ScrollMultiplierEvent sm)
         : base(parent, sm)
     {
     }
 
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+        originalPadding = Padding;
+        expandedPadding = originalPadding with { Left = originalPadding.Left + 4, Right = originalPadding.Right + 4 };
+    }
+
     protected override void Update()
     {
         base.Update();
-        Text.Text = $"{ScrollMultiplier.Multiplier.ToStringInvariant("0.####")}x {Math.Floor(ScrollMultiplier.Duration)}ms";
+        
+        if (isHovered)
+            Text.Text = $"{ScrollMultiplier.Multiplier.ToStringInvariant("0.####")}x {Math.Floor(ScrollMultiplier.Duration)}ms | {ScrollMultiplier.Easing}";
+        else
+            Text.Text = $"{ScrollMultiplier.Multiplier.ToStringInvariant("0.####")}x {Math.Floor(ScrollMultiplier.Duration)}ms";
+    }
+
+    protected override bool OnHover(HoverEvent e)
+    {
+        isHovered = true;
+        this.TransformTo(nameof(Padding), expandedPadding, 200, Easing.OutQuart);
+        return base.OnHover(e);
+    }
+
+    protected override void OnHoverLost(HoverLostEvent e)
+    {
+        isHovered = false;
+        this.TransformTo(nameof(Padding), originalPadding, 200, Easing.OutQuart);
+        base.OnHoverLost(e);
     }
 
     protected override bool OnClick(ClickEvent e)
