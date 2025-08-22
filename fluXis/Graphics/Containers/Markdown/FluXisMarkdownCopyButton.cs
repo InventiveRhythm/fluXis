c329c61d85ed3ab2b3d6e2eee82059d2270f8860
menvae <menvaedev@gmail.com>
2025-08-22 15:39:59 +0300

    implement custom tooltip instead of using toolbar button

--- fluXis/Graphics/Containers/Markdown/FluXisMarkdownCopyButton.cs
+++ fluXis/Graphics/Containers/Markdown/FluXisMarkdownCopyButton.cs
@@ -1,44 +1,42 @@
 using osu.Framework.Platform;
 using Markdig.Syntax;
-using fluXis.Overlay.Toolbar;
 using fluXis.Localization;
 using fluXis.Graphics.Sprites.Icons;
 using osu.Framework.Input.Events;
 using osu.Framework.Allocation;
 using fluXis.Overlay.Notifications;
+using fluXis.Graphics.UserInterface.Buttons;
+using osu.Framework.Graphics.Cursor;
+using fluXis.Graphics.Sprites.Text;
+using fluXis.Graphics.UserInterface.Text;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics;
+using osuTK;
+using fluXis.Overlay.Mouse;
 
 namespace fluXis.Graphics.Containers.Markdown;
 
-public partial class FluXisMarkdownCopyButton : ToolbarButton
+public partial class FluXisMarkdownCopyButton : IconButton, IHasCustomTooltip<FluXisMarkdownCopyButton>
 {
     [Resolved]
     private Clipboard clipboard { get; set; }
 
-    private CodeBlock codeBlock;
+    [Resolved]
+    private NotificationManager notifications { get; set; }
 
-    private NotificationManager notifications;
+    private readonly CodeBlock codeBlock;
+    private readonly CopyButtonTooltip tooltip;
+
+    public FluXisMarkdownCopyButton TooltipContent => this;
 
     public FluXisMarkdownCopyButton(CodeBlock codeBlock)
     {
         this.codeBlock = codeBlock;
+        tooltip = new CopyButtonTooltip();
 
-        RequireLogin = false;
-        TooltipTitle = LocalizationStrings.General.CopyToClipboard;
-        TooltipSub = LocalizationStrings.General.CopyToClipboardDescription;
         Icon = FontAwesome6.Solid.Clipboard;
-    }
-
-    [BackgroundDependencyLoader]
-    private void load(NotificationManager notifications)
-    {
-        this.notifications = notifications;
-    }
-
-    protected override void LoadComplete()
-    {
-        base.LoadComplete();
-
-        Enabled.Value = true;
+        IconSize = 24;
+        ButtonSize = 46;
     }
 
     protected override bool OnClick(ClickEvent e)
@@ -46,5 +44,55 @@ public partial class FluXisMarkdownCopyButton : ToolbarButton
         clipboard.SetText(string.Join("\n", codeBlock.Lines));
         notifications.SendSmallText("Copied text to Clipboard", FontAwesome6.Solid.Clipboard);
         return base.OnClick(e);
+    }
+
+    public ITooltip<FluXisMarkdownCopyButton> GetCustomTooltip() => tooltip;
+
+    private partial class CopyButtonTooltip : CustomTooltipContainer<FluXisMarkdownCopyButton>
+    {
+        public CopyButtonTooltip()
+        {
+            Child = new FillFlowContainer
+            {
+                AutoSizeAxes = Axes.Both,
+                Direction = FillDirection.Vertical,
+                Margin = new MarginPadding { Horizontal = 10, Vertical = 6 },
+                Children = new Drawable[]
+                {
+                    new FillFlowContainer
+                    {
+                        AutoSizeAxes = Axes.Both,
+                        Direction = FillDirection.Horizontal,
+                        Spacing = new Vector2(5),
+                        Children = new Drawable[]
+                        {
+                            new FluXisSpriteIcon
+                            {
+                                Size = new Vector2(16),
+                                Margin = new MarginPadding(4),
+                                Anchor = Anchor.CentreLeft,
+                                Origin = Anchor.CentreLeft,
+                                Icon = FontAwesome6.Solid.Clipboard
+                            },
+                            new FluXisSpriteText
+                            {
+                                FontSize = 24,
+                                Anchor = Anchor.CentreLeft,
+                                Origin = Anchor.CentreLeft,
+                                Text = LocalizationStrings.General.CopyToClipboard
+                            }
+                        }
+                    },
+                    new FluXisTextFlow
+                    {
+                        AutoSizeAxes = Axes.Both,
+                        FontSize = 18,
+                        Text = LocalizationStrings.General.CopyToClipboardDescription
+                    }
+                }
+            };
+        }
+
+        public override void SetContent(FluXisMarkdownCopyButton content) {} // Already set in constuctor so, this is pretty much implemented for the sake of it.
     }
 }
