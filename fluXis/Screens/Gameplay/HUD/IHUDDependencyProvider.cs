ffc97e7d0fe7e0b3da95e6f8a27f6e559efe20b6
flustix <me@flux.moe>
2025-08-22 22:16:06 +0200

    add kps counter hud element

--- fluXis/Screens/Gameplay/HUD/IHUDDependencyProvider.cs
+++ fluXis/Screens/Gameplay/HUD/IHUDDependencyProvider.cs
@@ -3,11 +3,14 @@ using fluXis.Map;
 using fluXis.Scoring;
 using fluXis.Scoring.Processing;
 using fluXis.Scoring.Processing.Health;
+using fluXis.Screens.Gameplay.Ruleset;
 
 namespace fluXis.Screens.Gameplay.HUD;
 
 public interface IHUDDependencyProvider
 {
+    RulesetContainer Ruleset { get; }
+
     JudgementProcessor JudgementProcessor { get; }
     HealthProcessor HealthProcessor { get; }
     ScoreProcessor ScoreProcessor { get; }
