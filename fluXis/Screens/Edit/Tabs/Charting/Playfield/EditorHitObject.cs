e05bba506cb8f54b65f2b94864f7420ad6479723
flustix <me@flux.moe>
2026-02-13 20:04:45 +0100

    fix hitobject unloading in the editor not using zoom

--- fluXis/Screens/Edit/Tabs/Charting/Playfield/EditorHitObject.cs
+++ fluXis/Screens/Edit/Tabs/Charting/Playfield/EditorHitObject.cs
@@ -24,7 +24,7 @@ public abstract partial class EditorHitObject : CompositeDrawable
 
     public HitObject Data { get; }
 
-    public virtual bool Visible => Math.Abs(EditorClock.CurrentTime - Data.Time) <= 2000;
+    public virtual bool Visible => Math.Abs(EditorClock.CurrentTime - Data.Time) <= 2000 / settings.Zoom;
 
     private FluXisSpriteText text { get; set; }
 
