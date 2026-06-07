1f3d4cbf24cf4523feb5809997fac43b7a487d38
flustix <me@flux.moe>
2026-06-07 16:36:42 +0200

    fix overlay not updating

--- fluXis/Screens/Gameplay/Capabilities/SpectatorCapability.cs
+++ fluXis/Screens/Gameplay/Capabilities/SpectatorCapability.cs
@@ -26,6 +26,7 @@ public partial class SpectatorCapability : ReplayCapability
         base.PostLoad();
         Overlay.Title = "Spectator Mode";
         Overlay.SubTitle = u => $"Watching {u?.Username}";
+        Overlay.Recreate();
     }
 
     protected override void ModifyRuleset(ReplayRulesetContainer ruleset)
